ee8f3a037494439f20c128471aaac32308f5611d
ra-fletcher <79005140+ra-fletcher@users.noreply.github.com>
2023-01-13 13:36:53 +1100

    Update cohort_studies_III.R

--- 04_cohort_studies_III/code/cohort_studies_III.R
+++ 04_cohort_studies_III/code/cohort_studies_III.R
@@ -14,8 +14,8 @@
 
 # This is the code for Practical Session 4: Analysis of Cohort Studies III
 
-# `|>` is the base R pipe operator. It will be used throughout this code in 
-# favour of the pipe operator imported with the magrittr library `%>%`
+# `|>` is the base R pipe operator. It will be used throughout this code instead
+# of the pipe operator imported with the magrittr library `%>%`
 
 
 # Install dependencies (if required) --------------------------------------
